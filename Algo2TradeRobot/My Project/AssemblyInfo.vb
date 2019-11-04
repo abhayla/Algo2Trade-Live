51802e2ddc7a4220c274debff0692be5d145d17c
Indibar <indibar@algo2trade.com>
2019-11-04 07:06:13 +0530

    After GUID and version change for sriji

--- Algo2TradeRobot/My Project/AssemblyInfo.vb
+++ Algo2TradeRobot/My Project/AssemblyInfo.vb
@@ -18,7 +18,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("d133c3cd-1d0e-41dc-a51f-677a88efaed5")>
+<Assembly: Guid("26ffb1ac-f915-4eca-85f0-8d4825854814")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("2.0.0.3")>
-<Assembly: AssemblyFileVersion("2.0.0.3")>
+<Assembly: AssemblyVersion("2.0.0.4")>
+<Assembly: AssemblyFileVersion("2.0.0.4")>
